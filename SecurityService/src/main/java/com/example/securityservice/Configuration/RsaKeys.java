0eb9a68224cc84fa0500f0a0abb9542c69926f5f
Asmae <asmae@gmail.com>
2025-11-10 00:11:17 +0100

    DONE

--- SecurityService/src/main/java/com/example/securityservice/Configuration/RsaKeys.java
+++
@@ -1,7 +0,0 @@
-package com.example.securityservice.Configuration;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.core.io.Resource;
-
-@ConfigurationProperties(prefix = "rsa")
-public record RsaKeys(Resource publicKey, Resource privateKey) { }
--- SecurityService/src/main/java/com/example/securityservice/Repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.securityservice.Repository;
-
-import com.example.securityservice.Entity.Utilisateur;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<Utilisateur, Integer> {
-    Utilisateur findByUsername(String username);
-}
